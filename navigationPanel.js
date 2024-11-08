1a2e47057b4490de3236bae097c893d9823993eb
PrivacySecured <LoveNewfies@proton.me>
2024-11-07 21:10:20 -0600

    add

--- navigationPanel.js
+++ navigationPanel.js
@@ -3,13 +3,12 @@ class NavPanel extends HTMLElement {
         this.innerHTML = `
         <div class="menu">
             <i class="fa-solid fa-house active" id="home"></i>
-            <i class="fa-brands fa-discord" id="discord"></i>
-            <i class="fa-brands fa-patreon" id="patreon"></i>
-            <i class="fa-solid fa-award" id="benefits"></i>
             <i class="fa-brands fa-youtube" id="youtube"></i>
             <i class="fa-brands fa-twitch" id="twitch"></i>
+            <i class="fa-brands fa-discord" id="discord"></i>
+            <i class="fa-brands fa-patreon" id="patreon"></i>
             <i class="fa-solid fa-dollar-sign" id="money"></i>
-            <i class="fa-solid fa-plus rightside" id="more"></i>
+            <i class="fas fa-info-circle rightside" id="additional"></i>
         </div>
       `;
     }
