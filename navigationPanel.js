fefd694050b043c5bcd7fe204142f98743067e95
PrivacySecured <LoveNewfies@proton.me>
2024-11-07 20:15:22 -0600

    setting up

---
+++ navigationPanel.js
@@ -0,0 +1,18 @@
+class NavPanel extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <div class="menu">
+            <i class="fa-solid fa-house active" id="home"></i>
+            <i class="fa-brands fa-youtube" id="youtube"></i>
+            <i class="fa-brands fa-twitch" id="twitch"></i>
+            <i class="fa-brands fa-discord" id="discord"></i>
+            <i class="fa-brands fa-patreon" id="patreon"></i>
+            <i class="fa-solid fa-award" id="benefits"></i>
+            <i class="fa-solid fa-dollar-sign" id="money"></i>
+            <i class="fa-solid fa-plus rightside" id="more"></i>
+        </div>
+      `;
+    }
+}
+
+customElements.define('nav-panel', NavPanel);
---
+++ res/app.js
@@ -0,0 +1,116 @@
+
+
+particlesJS('particles-js',
+  
+  {
+    "particles": {
+      "number": {
+        "value": 96,
+        "density": {
+          "enable": true,
+          "value_area": 1100
+        }
+      },
+      "color": {
+        "value": "#aa1111"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 2,
+          "color": "#242424"
+        },
+        "polygon": {
+          "nb_sides": 3
+        },
+        "image": {
+          "src": "https://avatars.githubusercontent.com/u/181297013?s=48&v=4",
+          "width": 5,
+          "height": 5
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": true,
+        "anim": {
+          "enable": true,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 12,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 20,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#383838",
+        "opacity": 0,
+        "width": 2.5252724532232724
+      },
+      "move": {
+        "enable": true,
+        "speed": 1.603412060865523,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "bubble"
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 133.99274002972194,
+          "size": 15,
+          "duration": 2.1926084732136317,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  }
+
+);
