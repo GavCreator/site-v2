df6db8e402927a9867c61df3ea649a0fd351327d
Newfie <157941859+Newfies@users.noreply.github.com>
2025-05-27 16:29:38 -0500

    dx

--- navigationPanel.js
+++ navigationPanel.js
@@ -9,7 +9,7 @@ class NavPanel extends HTMLElement {
             <i class="fa-brands fa-patreon" id="patreon"></i>
             <i class="fa-solid fa-shirt" id="shirt"></i>
             <i class="fa-solid fa-dollar-sign" id="money"></i>
-            <i class="fas fa-info-circle rightside" id="additional"></i>
+            <!-- <i class="fas fa-info-circle rightside" id="additional"></i> -->
         </div>
       `;
     }
