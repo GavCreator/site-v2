89baf1152c0032e145bda8deda6fbed3b2e299d6
Newfie <157941859+Newfies@users.noreply.github.com>
2025-06-25 08:59:57 -0500

    Update navigationPanel.js

--- navigationPanel.js
+++ navigationPanel.js
@@ -8,7 +8,9 @@ class NavPanel extends HTMLElement {
             <i class="fa-brands fa-discord" id="discord"></i>
             <i class="fa-brands fa-patreon" id="patreon"></i>
             <i class="fa-solid fa-shirt" id="shirt"></i>
-            <i><img src="https://images.greenmangaming.com/static/favicon.ico"></i> 
+            <i id="greenmangaming">
+                <img src="https://images.greenmangaming.com/static/favicon.ico" alt="GreenManGaming Icon">
+            </i>
             <i class="fa-solid fa-dollar-sign" id="money"></i>
             <!-- <i class="fas fa-info-circle rightside" id="additional"></i> -->
         </div>
